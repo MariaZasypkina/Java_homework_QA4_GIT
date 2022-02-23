108ce27c782b9e9fb4a23ea8f791c1dce456ef39
MariaZasypkina <maria.zasypkina@gmail.com>
2022-02-23 10:12:45 -0700

    HW1

---
+++ HW1.java
@@ -0,0 +1,7 @@
+public class HW1 {
+        public static void main(String[] args) {
+            System.out.println("1" + "\n" +"2" + "\n" + "3" + "\n" + "2" + "\n" + "Поехали!!!");
+        }
+    }
+
+
