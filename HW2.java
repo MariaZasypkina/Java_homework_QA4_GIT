ac94991bf331bf542c09119e450a07efe13a5586
MariaZ <maria.zasypkina@gmail.com>
2022-03-08 16:22:52 -0700

    HW2

---
+++ HW2.java
@@ -0,0 +1,19 @@
+public class HW2 {
+    public static void main(String[] args) {
+
+        int first = 124;
+        int second = 12;
+
+        System.out.println (first + second);
+        System.out.println (first - second);
+        System.out.println (first * second);
+        System.out.println (first / second);
+        System.out.println (first % second);
+
+        System.out.println("first is odd: " + first % 2);
+        System.out.println("second is odd: " + second % 2);
+
+    }
+
+}
+
