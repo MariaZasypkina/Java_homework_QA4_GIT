0696235d7fea820b5b74c928777628b1c0e41891
MariaZ <maria.zasypkina@gmail.com>
2022-03-25 19:16:01 -0600

    HW4

--- HW4_Task1_3.java
+++ HW4_Task1_3.java
@@ -8,8 +8,10 @@ public class HW4_Task1_3 {
      */
     public static void main(String[] args) {
         int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
-        int min = array[0];
 
+//        int min = array[0];    ver 1 but the following is more correct
+
+int min = Integer.MAX_VALUE;
         for (int i = 1; i < array.length; i++)
             if (array[i] < min)
                 min = array[i];
