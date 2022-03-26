633c4c45b742300802f46009c591ce5210d4bd65
MariaZ <maria.zasypkina@gmail.com>
2022-03-25 19:22:34 -0600

    HW4

--- HW4_Task1_4.java
+++ HW4_Task1_4.java
@@ -8,11 +8,10 @@ public class HW4_Task1_4 {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int sum = array[0];
-        int i;
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 56};
+        double sum = array[0];
 
-    for (i = 0; i < array.length; i++)
+    for (int i = 0; i < array.length; i++)
         sum += array[i];
     System.out.println(sum/ array.length);
     }
