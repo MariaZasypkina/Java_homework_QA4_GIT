18631eb38f92025c4e22c1f27778ee5b6122ff23
MariaZ <maria.zasypkina@gmail.com>
2022-03-28 14:32:48 -0600

    HW5

---
+++ HW5_Task1_kataYouCantCodeUnderPressure1.java
@@ -0,0 +1,11 @@
+public class HW5_Task1_kataYouCantCodeUnderPressure1 {
+/* 8 kyu
+You Can't Code Under Pressure #1
+Code as fast as you can! You need to double the integer and return it.
+ */
+class Java {
+    public static int doubleInteger(int i) {
+        return i*2;
+    }
+}
+    }
