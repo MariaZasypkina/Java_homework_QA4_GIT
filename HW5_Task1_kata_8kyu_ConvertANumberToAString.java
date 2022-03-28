5d0e79cb30f4ce89150ad5b025f5acca5de8a151
MariaZ <maria.zasypkina@gmail.com>
2022-03-28 15:37:05 -0600

    HW5

--- HW5_Task1_kata_8kyu_ConvertANumberToAString.java
+++ HW5_Task1_kata_8kyu_ConvertANumberToAString.java
@@ -2,4 +2,8 @@ public class HW5_Task1_kata_8kyu_ConvertANumberToAString {
     public static String numberToString(int num) {
         return "" + num;
     }
+
+    public static void main(String[] args) {
+        System.out.println(numberToString(6544));
+    }
 }
