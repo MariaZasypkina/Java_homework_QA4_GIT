d87119b11120728d8f0e44d3e8de6c4fbbb915c6
MariaZ <maria.zasypkina@gmail.com>
2022-03-28 15:53:08 -0600

    HW5

---
+++ HW5_1_kata_8kyu_FindTheSmallestIntegerInTheArray.java
@@ -0,0 +1,12 @@
+public class HW5_1_kata_8kyu_FindTheSmallestIntegerInTheArray {
+    public static int findSmallestInt(int[] args) {
+        int min = args[0];
+        for (int i = 1; i < args.length; i++) {
+            if (min > args[i]) {
+                min = args[i];
+            }
+        }
+        return min;
+
+    }
+}
