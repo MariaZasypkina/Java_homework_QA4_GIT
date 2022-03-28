ae031315a94025d11da0b938af49f66f630577db
MariaZ <maria.zasypkina@gmail.com>
2022-03-28 15:31:37 -0600

    HW5

--- HW5_Task1_kataYouCantCodeUnderPressure1.java
+++ HW5_Task1_kataYouCantCodeUnderPressure1.java
@@ -3,9 +3,13 @@ public class HW5_Task1_kataYouCantCodeUnderPressure1 {
 You Can't Code Under Pressure #1
 Code as fast as you can! You need to double the integer and return it.
  */
-class Java {
+
     public static int doubleInteger(int i) {
         return i*2;
     }
-}
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(178));
     }
+}
+
