b29440803b90379b1659dad3efa2e60395710dba
MariaZ <maria.zasypkina@gmail.com>
2022-03-29 11:37:49 -0600

    HW5

---
+++ HW5_Task2_1.java
@@ -0,0 +1,24 @@
+public class HW5_Task2_1 {
+    public static void main(String[] args) {
+for (int i = 9; i>=0; i--) {
+    for (int j = 0; j <= i; j++) {
+        System.out.print(j + " ");
+    }
+    System.out.println();
+}
+}
+}
+
+
+
+
+//        0  1  2  3  4  5  6  7  8  9
+//        0  1  2  3  4  5  6  7  8
+//        0  1  2  3  4  5  6  7
+//        0  1  2  3  4  5  6
+//        0  1  2  3  4  5
+//        0  1  2  3  4
+//        0  1  2  3
+//        0  1  2
+//        0  1
+//        0
