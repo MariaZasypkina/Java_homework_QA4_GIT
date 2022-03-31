6ae60dee250f1819515b5cafdccf52754744dad8
MariaZ <maria.zasypkina@gmail.com>
2022-03-31 11:50:56 -0600

    Study test

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static int myMethod(int a, int b) {
+        return (a + b) * a;
+    }
+    public static void main(String[] args) {
+        System.out.println(myMethod(23, 67));
+    }
+}
