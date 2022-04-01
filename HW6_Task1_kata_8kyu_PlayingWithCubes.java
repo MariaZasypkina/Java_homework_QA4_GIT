e754d2f279db9d1efb4898d920049c1d822594e1
MariaZ <maria.zasypkina@gmail.com>
2022-03-31 19:45:42 -0600

    Study test

---
+++ HW6_Task1_kata_8kyu_PlayingWithCubes.java
@@ -0,0 +1,12 @@
+public class HW6_Task1_kata_8kyu_PlayingWithCubes {
+    //public class Cube
+    private int Side;
+    public int getSide(){
+        return Side;
+    }
+    public int setSide(int num){
+        return this.Side = num;
+    }
+
+}
+}
