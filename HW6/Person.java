1ce421476246c3a58dc275087294f0723dcfe119
MariaZ <maria.zasypkina@gmail.com>
2022-04-14 15:56:27 -0600

    HW6

---
+++ HW6/Person.java
@@ -0,0 +1,14 @@
+package HW6;
+
+public class Person {
+    //Задача №1. //Необходимо создать класс Person с полями: имя, возраст, пол. Класс должен иметь метод -
+    // getName, метод возвращает имя с префиксом “Mr. ” если пол указан как мужской и префикс “Mrs. ”
+    // если женский.
+private int name;
+private int age;
+private String gender;
+
+
+
+
+}
--- HW6_2_1.java
+++
@@ -1,2 +0,0 @@
-public class HW6_2_1 {
-}
